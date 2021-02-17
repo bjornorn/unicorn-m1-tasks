a7669416861a1a0f9a52b3e97c96c095d6ecac86
dinastene <76996968+dinastene@users.noreply.github.com>
2021-02-17 14:54:16 +0100

    hei

--- uke7/bossFight.js
+++ uke7/bossFight.js
@@ -1,23 +1,23 @@
 function attackChoice() {
-  attackNumber = Math.floor(Math.random() * 3);
-  //   console.log(attackNumber);
+  attackNumber = Math.floor(Math.random() * 4);
+    console.log('attack ' + attackNumber);
+
 }
 
 function playerIsAttacking() {
   attackChoice();
-  computer.health = computer.health - player.attack[attackNumber];
-  console.log(computer.health);
+  computer.health = computer.health - player.attack[attackNumber];  
+  if (computer.health < 1) {theWinnerIs()}
   view();
   //   setTimeout(computerIsAttacking, 2000);
   computerIsAttacking();
-  computer.health < 1 ? theWinnerIs() : null;
 }
 
 function computerIsAttacking() {
   attackChoice();
-  player.health = computer.health - computer.attack[attackNumber];
+  player.health = player.health - computer.attack[attackNumber];
+  if (player.health < 1) {theWinnerIs()}
   view();
-  player.health < 1 ? theWinnerIs() : null;
 }
 
 function theWinnerIs() {
@@ -26,12 +26,22 @@ function theWinnerIs() {
   if (player.health < 1) {
     winner = computer.name;
     winnerMessage = 'Computer Wins!';
-    view();
+    restartMessage = 'Restart'; 
+  
   }
   if (computer.health < 1) {
     winner = player.name;
     winnerMessage = 'Player Wins!';
-    view();
+
   }
-  console.log(winner);
+  // console.log(winner);
+}
+
+function restart() {
+  winnerMessage = '';
+  restartMessage = '';
+  player.health = player.startHealth;
+  computer.health = computer.startHealth;
+  winner = null;
+  view();
 }
