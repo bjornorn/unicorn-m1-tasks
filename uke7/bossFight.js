d7f6d7077c4333333b14dc44d6b456c8a9d0eb23
bjornorn <bjorn.ornevik@hotmail.com>
2021-02-18 15:51:17 +0100

    BossFight v 0.9

--- uke7/bossFight.js
+++ uke7/bossFight.js
@@ -1,22 +1,34 @@
 function attackChoice() {
   attackNumber = Math.floor(Math.random() * 4);
-    console.log('attack ' + attackNumber);
-
+  console.log('attack ' + attackNumber);
 }
 
 function playerIsAttacking() {
+  playerClass = 'playerFiring';
+  winner = 'unknown';
   attackChoice();
-  computer.health = computer.health - player.attack[attackNumber];  
-  if (computer.health < 1) {theWinnerIs()}
   view();
-  //   setTimeout(computerIsAttacking, 2000);
-  computerIsAttacking();
+  setTimeout(computerIsAttacking, 600);
 }
+// computerIsAttacking();
 
 function computerIsAttacking() {
+  computer.health = computer.health - player.attack[attackNumber];
+  if (computer.health < 1) {
+    theWinnerIs();
+  }
   attackChoice();
   player.health = player.health - computer.attack[attackNumber];
-  if (player.health < 1) {theWinnerIs()}
+  if (player.health < 1) {
+    theWinnerIs();
+  }
+  view();
+  fightersIsResting();
+}
+
+function fightersIsResting() {
+  playerClass = 'playerIdle';
+  winner = null;
   view();
 }
 
@@ -26,15 +38,21 @@ function theWinnerIs() {
   if (player.health < 1) {
     winner = computer.name;
     winnerMessage = 'Computer Wins!';
-    restartMessage = 'Restart'; 
-  
+    restartMessage = true;
   }
   if (computer.health < 1) {
     winner = player.name;
     winnerMessage = 'Player Wins!';
-
+    restartMessage = true;
   }
   // console.log(winner);
+}
+
+function startGame() {
+  startMessage = false;
+  let winner = null;
+  playerClass = 'idle';
+  restart();
 }
 
 function restart() {
