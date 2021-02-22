899f55564bfe870b99a6a4d885274f51a9b8f528
Agith-Ravn <76996657+Agith-Ravn@users.noreply.github.com>
2021-02-22 12:48:38 +0100

    .

--- uke7/bossFight.js
+++ uke7/bossFight.js
@@ -1,48 +1,46 @@
 function attackChoice() {
   attackNumber = Math.floor(Math.random() * 4);
-  // console.log('attack ' + attackNumber);
 }
 
 function playerIsAttacking() {
   playerClass = 'playerFiring'; //aktiver skyte animasjon
   allowAttack = 'no'; //deaktiver onclick for å angripe
   attackChoice(); // lag et tilfeldig tall mellom 1 og 4
+  computerSpeech = '';
+  playerAttackSound.play();
   view(); //oppdater view
-  console.log(playerClass);
   setTimeout(computerIsAttacking, 1200); //vent x antall tusendels-sekunder og start neste funksjon
 }
-// computerIsAttacking();
+//computerIsAttacking();
 
 function computerIsAttacking() {
   attackChoice();
-  playerClass = 'playerIdle'; // lag et tilfeldig tall mellom 1 og 4
+  playerClass = 'playerIdle'; //lag et tilfeldig tall mellom 1 og 4
   computerClass = 'computerFiring';
+  computerAttackSound.play();
   computer.health = computer.health - player.attack[attackNumber]; //reduser computer sitt liv iht "treff"
   if (computer.health < 1) {
-    // sjekk om computer er død?
     theWinnerIs();
   }
 
   view(); //oppdater view
   if (computer.health > 0 && player.health > 0) {
-    // dersom begge lever
-    setTimeout(fightersIsResting, 560); // start neste funksjon(med forsinkelse)
+    //dersom begge lever
+    setTimeout(fightersIsResting, 560); //start neste funksjon(med forsinkelse)
   }
 }
 
 function fightersIsResting() {
-  computerClass = 'computerIdle'; // start idle animasjon
+  computerClass = 'computerIdle'; //start idle animasjon
   allowAttack = 'yes'; //aktiver onclick for å angripe
-  player.health = player.health - computer.attack[attackNumber]; // sjekk om player er død?
+  player.health = player.health - computer.attack[attackNumber]; //sjekk om player er død?
   if (player.health < 1) {
     theWinnerIs();
   }
-  view(); //oppdater view
+  view();
 }
 
 function theWinnerIs() {
-  // console.log(player.health);
-
   if (player.health < 1) {
     winner = computer.name;
     restartMessage = true;
@@ -50,6 +48,10 @@ function theWinnerIs() {
     computerClass = 'computerIdle';
     gameTitle = 'yes';
     allowAttack = 'no';
+    computerSpeech = '';
+    playerSpeech = '';
+    gameThemeAudio.pause();
+    audioComputerVictory.play();
   }
 
   if (computer.health < 1) {
@@ -59,8 +61,12 @@ function theWinnerIs() {
     computerClass = 'computerIdle';
     gameTitle = 'yes';
     allowAttack = 'no';
+    computerSpeech = '';
+    playerSpeech = '';
+    gameThemeAudio.pause();
+    audioPlayerVictory2.play();
   }
-  // console.log(winner);
+  view();
 }
 
 function startGame() {
@@ -68,12 +74,10 @@ function startGame() {
   startMessage = false;
   playerClass = 'playerIdle';
   computerClass = 'computerIdle';
-
   restart();
 }
 
 function restart() {
-  // winnerMessage = '';
   restartMessage = '';
   playerClass = 'playerIdle';
   player.health = player.startHealth;
@@ -81,11 +85,22 @@ function restart() {
   allowAttack = 'yes';
   gameTitle = 'yes';
   winner = null;
-
+  computerSpeech = 'computerSpeech';
+  player.attack = [5, 10, 10, 20];
+  audioPlayerVictory2.pause();
+  audioComputerVictory.pause();
+  gameTheme();
   view();
 }
 
 function gameTheme() {
-  gameThemeAudio.volume = 0.5;
+  gameThemeAudio.currentTime = 0;
+  gameThemeAudio.volume = 0.3;
   gameThemeAudio.play();
+}
+
+function jukseKnapp() {
+  player.attack = [40, 40, 40, 40];
+  playerSpeech = 'playerSpeech';
+  view();
 }
